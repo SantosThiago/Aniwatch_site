4b91497b7655d7d1df15d6e59a674aa5ea26a6ae
Thiago dos Santos Silva <th_santos@id.uff.br>
2023-03-22 21:25:55 +0000

    Update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var submit=document.getElementById('submit');
-var container=document.getElementById("container");
-const proxy="https://cors-anywhere.herokuapp.com/"
+var submit = document.getElementById('submit');
+var container = document.getElementById("container");
+const proxy = "https://cors-anywhere.herokuapp.com/"
 const url = 'https://graphql.anilist.co';
 const menu_query =
-`
+  `
   query
   {
     Page (perPage: 40)
@@ -39,7 +39,7 @@ const menu_query =
   }
 `;
 const query =
-`
+  `
   query ($search: String) 
   {
     Page (perPage: 40) 
@@ -77,299 +77,288 @@ const query =
     }
   }
 `;
-var menu_options = 
+var menu_options =
 {
   method: 'POST',
-  headers: 
+  headers:
   {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
   body: JSON.stringify(
-  {
-    query: menu_query
-  })
+    {
+      query: menu_query
+    })
 };
 
 document.addEventListener(
   "keypress",
-  (e) => 
-  {
-   if (e.key==='Enter')
-   {
+  (e) => {
+    if (e.key === 'Enter') {
       let button = document.querySelector("#submit");
       button.click();
-   }
+    }
   });
 
 submit.addEventListener(
   'click',
-  (e) =>
-  {
+  (e) => {
     e.preventDefault();
     document.querySelectorAll('.card').forEach(e => e.remove());
-    var variables={'search':anime_search.value};
-    var options = 
+    var variables = { 'search': anime_search.value };
+    var options =
     {
       method: 'POST',
-      headers: 
+      headers:
       {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
       },
       body: JSON.stringify(
-      {
-        query: query,
-        variables: variables
-      })
+        {
+          query: query,
+          variables: variables
+        })
     };
-    request_API(url,options,true);
+    request_API(url, options, true);
   });
 
-function check_Links(urls,title)
-{
-  let new_urls=[];
-  
-  for (let i in urls)
-  {
-    elem=urls[i];
-    let link=elem['url'];
-    let streamingName=elem['site'];
-    if (streamingName=='Crunchyroll')
-    {
+function check_Links(urls, title) {
+  let new_urls = [];
+
+  for (let i in urls) {
+    elem = urls[i];
+    let link = elem['url'];
+    let streamingName = elem['site'];
+    if (streamingName == 'Crunchyroll') {
       new_urls.push(urls[i]);
     }
 
-    else if (streamingName=='Crunchyroll')
-    {
+    else if (streamingName == 'Crunchyroll') {
       //Problem: return title Just a moment... :(
       new_urls.push(urls[i]);
     }
-      
-    else if (streamingName=='Star+')
-    {
+
+    else if (streamingName == 'Star+') {
       new_urls.push(urls[i]);
     }
-    
-    else if (streamingName=='Disney Plus' && title=='Star Wars: Visions')
-    {
+
+    else if (streamingName == 'Disney Plus' && title == 'Star Wars: Visions') {
       new_urls.push(urls[i]);
     }
-  
-    else if (streamingName=='Netflix')
-    {
+
+    else if (streamingName == 'Netflix') {
       let xhttp = new XMLHttpRequest();
-      xhttp.open("GET", proxy+link, false);
+      xhttp.open("GET", proxy + link, false);
       xhttp.send();
       let tpl = document.createElement('template');
       tpl.innerHTML = xhttp.responseText;
       let fragment = tpl.content;
-      scrapTitle1=fragment.children[0]['text'];
-      scrapTitle2=fragment.children[4]['text'];
-      if (scrapTitle1==undefined && scrapTitle2!=undefined && scrapTitle2.length>7)
-        new_urls.push(urls[i]);
-    }
-      
-    else if (streamingName=='HBO Max')
-    {
-      let xhttp = new XMLHttpRequest();
-      xhttp.open("GET", proxy+link, false);
-      xhttp.send();
-      let tpl = document.createElement('template');
-      tpl.innerHTML = xhttp.responseText;
-      let fragment = tpl.content;
-      scrapTitle1=fragment.children[15]['text'];
-      scrapTitle2=fragment.children[3]['text'];
-      if (scrapTitle1==undefined && scrapTitle2!=undefined && scrapTitle2.length>7)
+      scrapTitle1 = fragment.children[0]['text'];
+      scrapTitle2 = fragment.children[4]['text'];
+      if (scrapTitle1 == undefined && scrapTitle2 != undefined && scrapTitle2.length > 7)
         new_urls.push(urls[i]);
     }
 
-    else if (streamingName=='Amazon')
-    {
-      new_urls.push(urls[i]);
+    else if (streamingName == 'HBO Max') {
+      let xhttp = new XMLHttpRequest();
+      xhttp.open("GET", proxy + link, false);
+      xhttp.send();
+      let tpl = document.createElement('template');
+      tpl.innerHTML = xhttp.responseText;
+      let fragment = tpl.content;
+      scrapTitle1 = fragment.children[15]['text'];
+      scrapTitle2 = fragment.children[3]['text'];
+      if (scrapTitle1 == undefined && scrapTitle2 != undefined && scrapTitle2.length > 7)
+        new_urls.push(urls[i]);
+    }
+
+    else if (streamingName == 'Amazon') {
+      if (link.indexOf('primevideo') != -1) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", proxy + link, false);
+        xhttp.send();
+        response = xhttp.responseText;
+        notFound = response.indexOf("Not available");
+        if (notFound != -1)
+          new_urls.push(urls[i]);
+      }
+
+      else if (link.indexOf('amazon') != -1) {
+        title = title.replace(/\s/g, '%20');
+        let search = 'www.primevideo.com/search/ref=atv_nb_sr?phrase=' + title + '&ie=UTF8';
+        //console.log(search);
+        //console.log(proxy+search);
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", proxy + search, false);
+        xhttp.send();
+        response = xhttp.responseText;
+        console.log(response);
+
+        new_urls.push(urls[i]);
+      }
     }
   }
   return new_urls;
 }
 
-function request_API(url,opt,search)
-{
-  if (search==true)
-  {
+function request_API(url, opt, search) {
+  if (search == true) {
     fetch(url, opt).then(handleResponse)
-                       .then(searchData)
-                       .catch(handleError);
+      .then(searchData)
+      .catch(handleError);
   }
-  else
-  {
+  else {
     fetch(url, opt).then(handleResponse)
-                     .then(menuData)
-                     .catch(handleError);
+      .then(menuData)
+      .catch(handleError);
   }
 }
 
-function handleResponse(response) 
-{
-  return response.json().then(function (json)
-  {
+function handleResponse(response) {
+  return response.json().then(function(json) {
     return response.ok ? json : Promise.reject(json);
   });
 }
 
-function searchData(data) 
-{
-  let mediaInfo=data['data']['Page']['media'];
-  
-  for (let value in mediaInfo)
-  {
-    let elem=mediaInfo[value];
-    let episodes=elem['episodes'];
-    let format=elem['format'];
-    let duration=elem['duration'];
-    let image=elem['coverImage']['large'];
-    let romaji=elem['title']['romaji'];
-    let externalLinks=elem['externalLinks'];
-    let anilistUrl=elem['siteUrl'];
-    externalLinks=check_Links(externalLinks,romaji);
-    add_Card(image,romaji,externalLinks,anilistUrl,episodes,format,duration);
+function searchData(data) {
+  let mediaInfo = data['data']['Page']['media'];
+
+  for (let value in mediaInfo) {
+    let elem = mediaInfo[value];
+    let episodes = elem['episodes'];
+    let format = elem['format'];
+    let duration = elem['duration'];
+    let image = elem['coverImage']['large'];
+    let romaji = elem['title']['romaji'];
+    let externalLinks = elem['externalLinks'];
+    let anilistUrl = elem['siteUrl'];
+    externalLinks = check_Links(externalLinks, romaji);
+    add_Card(image, romaji, externalLinks, anilistUrl, episodes, format, duration);
   }
 }
 
-function handleError(error) 
-{
+function handleError(error) {
   console.log('Error!');
   console.error(error);
 }
 
-function nodeFactory(type,content,name)
-{
-    var node=document.createElement(type);
-    if (type=='img')
-    {
-      node.src=content;
-      node.id=name;
-      return node;
-    }
-      
-    else if (type=='p')
-    {
-      node.innerHTML=content;
-      node.id=name;
-      return node;
-    }
+function nodeFactory(type, content, name) {
+  var node = document.createElement(type);
+  if (type == 'img') {
+    node.src = content;
+    node.id = name;
+    return node;
+  }
 
-    else if (type=='a')
-    {
-      node.id=content;
-      return node;
-    }
+  else if (type == 'p') {
+    node.innerHTML = content;
+    node.id = name;
+    return node;
+  }
 
-    else if (type=='div')
-    {
-      if (content=='carda')
-        console.log(content);
-      node.className=content;
-      return node;
-    }
+  else if (type == 'a') {
+    node.id = content;
+    return node;
+  }
 
-    else
-      console.log("Tipo inválido");
-      return -1;
+  else if (type == 'div') {
+    if (content == 'carda')
+      console.log(content);
+    node.className = content;
+    return node;
+  }
+
+  else
+    console.log("Tipo inválido");
+  return -1;
 }
 
-function add_Card(image,romaji,externalLinks,anilistUrl,episodes,format,duration)
-{
-  let card=nodeFactory('div','card');
-  let imgElement=nodeFactory('img',image,'coverImage');
-  let imgUrl=nodeFactory('a','anilistUrl');
-  let titleElement=nodeFactory('p',romaji,'title');
-  let streamingsTitle=nodeFactory('p','Streamings:','streamingsTitle');
-  let episodesTitle=nodeFactory('p','Episódios:','episodesTitle');
-  let episodesElement=nodeFactory('p',episodes,'episodesElement');
-  let formatTitle=nodeFactory('p','Formato:','formatTitle');
-  let formatElement=nodeFactory('p',format,'formatElement');
-  let durationTitle=nodeFactory('p','Duração:','durationTitle');
-  let durationElement=nodeFactory('p',duration+'min','durationElement');
-  let streamings=nodeFactory('div','streamings');
-  let cardInfo=nodeFactory('div','cardInfo');
-  imgUrl.href=anilistUrl;
-  imgUrl.target='_blank';
-  
-  
-  for (let linkInfo in externalLinks)
-  {
-    let elem=externalLinks[linkInfo];
-    let link=elem['url'];
-    let linkName=elem['site'];
-    
-    if (linkName=='Crunchyroll')
-    {
-      let streamingIcon=nodeFactory('img','Icons/crunchyrollIcon.png','streamingIcon');
-      let streamingsElement=nodeFactory('a','streaming');
-      streamingIcon.name='Crunchyroll';
-      streamingsElement.href=link;
-      streamingsElement.target='_blank';
-      streamingsElement.appendChild(streamingIcon);
-      streamings.appendChild(streamingsElement);
-    }
-      
-    else if (linkName=='Netflix')
-    {
-      let streamingIcon=nodeFactory('img','Icons/netflixIcon.svg','streamingIcon');
-      let streamingsElement=nodeFactory('a','streaming');
-      streamingIcon.name='Netflix';
-      streamingsElement.href=link;
-      streamingsElement.target='_blank';
+function add_Card(image, romaji, externalLinks, anilistUrl, episodes, format, duration) {
+  let card = nodeFactory('div', 'card');
+  let imgElement = nodeFactory('img', image, 'coverImage');
+  let imgUrl = nodeFactory('a', 'anilistUrl');
+  let titleElement = nodeFactory('p', romaji, 'title');
+  let streamingsTitle = nodeFactory('p', 'Streamings:', 'streamingsTitle');
+  let episodesTitle = nodeFactory('p', 'Episódios:', 'episodesTitle');
+  let episodesElement = nodeFactory('p', episodes, 'episodesElement');
+  let formatTitle = nodeFactory('p', 'Formato:', 'formatTitle');
+  let formatElement = nodeFactory('p', format, 'formatElement');
+  let durationTitle = nodeFactory('p', 'Duração:', 'durationTitle');
+  let durationElement = nodeFactory('p', duration + 'min', 'durationElement');
+  let streamings = nodeFactory('div', 'streamings');
+  let cardInfo = nodeFactory('div', 'cardInfo');
+  imgUrl.href = anilistUrl;
+  imgUrl.target = '_blank';
+
+
+  for (let linkInfo in externalLinks) {
+    let elem = externalLinks[linkInfo];
+    let link = elem['url'];
+    let linkName = elem['site'];
+
+    if (linkName == 'Crunchyroll') {
+      let streamingIcon = nodeFactory('img', 'Icons/crunchyrollIcon.png', 'streamingIcon');
+      let streamingsElement = nodeFactory('a', 'streaming');
+      streamingIcon.name = 'Crunchyroll';
+      streamingsElement.href = link;
+      streamingsElement.target = '_blank';
       streamingsElement.appendChild(streamingIcon);
       streamings.appendChild(streamingsElement);
     }
 
-    else if (linkName=='Disney Plus')
-    {
-      let streamingIcon=nodeFactory('img','Icons/disneyplusIcon.svg','streamingIcon');
-      let streamingsElement=nodeFactory('a','streaming');
-      streamingIcon.name='disneyplus';
-      streamingIcon.style.background="rgb(33,3,124)";
-      streamingsElement.href=link;
-      streamingsElement.target='_blank';
+    else if (linkName == 'Netflix') {
+      let streamingIcon = nodeFactory('img', 'Icons/netflixIcon.svg', 'streamingIcon');
+      let streamingsElement = nodeFactory('a', 'streaming');
+      streamingIcon.name = 'Netflix';
+      streamingsElement.href = link;
+      streamingsElement.target = '_blank';
       streamingsElement.appendChild(streamingIcon);
       streamings.appendChild(streamingsElement);
     }
 
-    else if (linkName=='Star+')
-    {
-      let streamingIcon=nodeFactory('img','Icons/starplusIcon.jpg','streamingIcon');
-      let streamingsElement=nodeFactory('a','streaming');
-      streamingIcon.name='starplus';
-      streamingsElement.href=link;
-      streamingsElement.target='_blank';
+    else if (linkName == 'Disney Plus') {
+      let streamingIcon = nodeFactory('img', 'Icons/disneyplusIcon.svg', 'streamingIcon');
+      let streamingsElement = nodeFactory('a', 'streaming');
+      streamingIcon.name = 'disneyplus';
+      streamingIcon.style.background = "rgb(33,3,124)";
+      streamingsElement.href = link;
+      streamingsElement.target = '_blank';
       streamingsElement.appendChild(streamingIcon);
       streamings.appendChild(streamingsElement);
     }
 
-    else if (linkName=='Amazon')
-    {
-      let streamingIcon=nodeFactory('img','Icons/primevideoIcon.webp','streamingIcon');
-      let streamingsElement=nodeFactory('a','streaming');
-      streamingIcon.name='amazon';
-      streamingsElement.href=link;
-      streamingsElement.target='_blank';
+    else if (linkName == 'Star+') {
+      let streamingIcon = nodeFactory('img', 'Icons/starplusIcon.jpg', 'streamingIcon');
+      let streamingsElement = nodeFactory('a', 'streaming');
+      streamingIcon.name = 'starplus';
+      streamingsElement.href = link;
+      streamingsElement.target = '_blank';
       streamingsElement.appendChild(streamingIcon);
       streamings.appendChild(streamingsElement);
     }
 
-    else if (linkName=='HBO Max')
-    {
-      let streamingIcon=nodeFactory('img','Icons/hbomaxIcon.webp','streamingIcon');
-      let streamingsElement=nodeFactory('a','streaming');
-      streamingIcon.name='hbomax';
-      streamingsElement.href=link;
-      streamingsElement.target='_blank';
+    else if (linkName == 'Amazon') {
+      let streamingIcon = nodeFactory('img', 'Icons/primevideoIcon.webp', 'streamingIcon');
+      let streamingsElement = nodeFactory('a', 'streaming');
+      streamingIcon.name = 'amazon';
+      streamingsElement.href = link;
+      streamingsElement.target = '_blank';
+      streamingsElement.appendChild(streamingIcon);
+      streamings.appendChild(streamingsElement);
+    }
+
+    else if (linkName == 'HBO Max') {
+      let streamingIcon = nodeFactory('img', 'Icons/hbomaxIcon.webp', 'streamingIcon');
+      let streamingsElement = nodeFactory('a', 'streaming');
+      streamingIcon.name = 'hbomax';
+      streamingsElement.href = link;
+      streamingsElement.target = '_blank';
       streamingsElement.appendChild(streamingIcon);
       streamings.appendChild(streamingsElement);
     }
   }
-  
+
   imgUrl.appendChild(imgElement);
   cardInfo.appendChild(episodesTitle);
   cardInfo.appendChild(episodesElement);
@@ -384,35 +373,31 @@ function add_Card(image,romaji,externalLinks,anilistUrl,episodes,format,duration
   card.appendChild(titleElement);
   container.appendChild(card);
 
-  if (streamings.childNodes.length==0)
-  {
-    let streamingIcon=nodeFactory('img','Icons/emojiIcon.png','streamingIcon');
-    let streamingsElement=nodeFactory('a','streaming');
-    streamingIcon.name='emojiIcon';
+  if (streamings.childNodes.length == 0) {
+    let streamingIcon = nodeFactory('img', 'Icons/emojiIcon.png', 'streamingIcon');
+    let streamingsElement = nodeFactory('a', 'streaming');
+    streamingIcon.name = 'emojiIcon';
     streamingsElement.appendChild(streamingIcon);
     streamings.appendChild(streamingsElement);
   }
 }
 
-function menuData(data)
-{
-  let mediaInfo=data['data']['Page']['media'];
-  
-  for (let value in mediaInfo)
-  {
-    let elem=mediaInfo[value];
-    let episodes=elem['episodes'];
-    let format=elem['format'];
-    let duration=elem['duration'];
-    let averageScore=elem['averageScore'];
-    let image=elem['coverImage']['large'];
-    let romaji=elem['title']['romaji'];
-    let externalLinks=elem['externalLinks'];
-    let anilistUrl=elem['siteUrl'];
+function menuData(data) {
+  let mediaInfo = data['data']['Page']['media'];
+
+  for (let value in mediaInfo) {
+    let elem = mediaInfo[value];
+    let episodes = elem['episodes'];
+    let format = elem['format'];
+    let duration = elem['duration'];
+    let averageScore = elem['averageScore'];
+    let image = elem['coverImage']['large'];
+    let romaji = elem['title']['romaji'];
+    let externalLinks = elem['externalLinks'];
+    let anilistUrl = elem['siteUrl'];
     //externalLinks=check_Links(externalLinks,romaji);
-    add_Card(image,romaji,externalLinks,anilistUrl,episodes,format,duration,averageScore);
+    add_Card(image, romaji, externalLinks, anilistUrl, episodes, format, duration, averageScore);
   }
 }
 
-
-request_API(url,menu_options,false);
+request_API(url, menu_options, false);
