42d7de19d19425526aff71b715f8d376efdcf0a2
Thiago <th_santos@id.uff.br>
2023-10-02 23:07:31 -0300

    v1.0.8

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const menu_query =
         lastPage,
         hasNextPage
       }
-      media (type: ANIME,isAdult: false, season: SUMMER, seasonYear: 2023, sort: SCORE_DESC)
+      media (type: ANIME,isAdult: false, season: FALL, seasonYear: 2023, sort: SCORE_DESC)
       {
         episodes
         format
